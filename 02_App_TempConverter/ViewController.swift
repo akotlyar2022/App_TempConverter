25929027d295f4d0f9edd613b64e0c898867d48c
AleksandrKotliar <sashagsmnew@gmail.com>
2022-11-05 19:34:56 +0400

    Initial Commit

---
+++ 02_App_TempConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  02_App_TempConverter
+//
+//  Created by macbook on 05.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
